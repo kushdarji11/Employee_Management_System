f97b72e6c7eaa34b25187e9cd9f11f7a3b52a861
Kush Darji <kushdarji11@gmail.com>
2022-11-19 00:08:36 -0500

    header/footer components created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import ListEmployeeComponent from './Components/ListEmployeeComponent';
+import HeaderComponents from './Components/HeaderComponents';
+import FooterComponent from './Components/FooterComponent';
 
 function App() {
   return (
-    <div className="container">
-      <ListEmployeeComponent />
+    <div>
+      <Router>
+          <HeaderComponents />
+          <div className="container">
+            <Switch>
+              <Route path='/' component={ListEmployeeComponent}></Route>
+              <Route path='/employees' component={ListEmployeeComponent}></Route>
+              <ListEmployeeComponent />
+            </Switch>
+          </div>
+          <FooterComponent />
+
+      </Router>
     </div>
+
   );
 }
 
